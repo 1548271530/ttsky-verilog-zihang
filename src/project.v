36ec035b03dedcd1e0c1b4c107ccf669bd286028
Zihang Cheng <czhicicle@gmail.com>
2026-04-25 12:50:16 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -6,7 +6,7 @@
 
 module tt_um_sobel #(
     parameter IMG_SIZE    = 16,
-    parameter OUTPUT_BITS = 4
+    parameter OUTPUT_BITS = 1
 )(
     input  wire [7:0] ui_in,
     output wire [7:0] uo_out,
